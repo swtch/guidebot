26e1c50f3ddd14d6b0ec834556e07563bd66343d
swtch <romar@semvolcans.com>
2018-04-24 17:44:27 +0200

    test

--- commands/eft.js
+++ commands/eft.js
@@ -1,32 +1,33 @@
 exports.run = async (client, message, [action, key, ...value], level) => { // eslint-disable-line no-unused-vars
 
     const tips = client.tips.get()
+    const game = 'tarkov'
 
     if (action === "add") {
         if (!key) return message.reply("Merci de préciser le \"Tips\" à ajouter");
-        if (eft[key]) return message.reply("Cette \"Tips\" existe déja");
+        if (tips[key]) return message.reply("Cette \"Tips\" existe déja");
         if (value.length < 1) return message.reply("Erreur, aucune valeur specifiée");
-        eft[key] = value.join(" ");
-        client.tips.set(message.guild.id, tips);
+        tips[key] = value.join(" ");
+        client.tips.set(game, tips);
         message.reply(`${key} à até ajouté avec comme valeur: ${value.join(" ")}`);
       } else
       if (action === "edit") {
         if (!key) return message.reply("Merci de préciser la \"Tips\" à editer");
         if (value.length < 1) return message.reply("Erreur, aucune valeur specifiée");
-        if (!settings[key]) return message.reply("Cette \"Tips\" n'existe pas");
-        eft[key] = value.join(" ");
-        client.settings.set(message.guild.id, settings);
+        if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
+        tips[key] = value.join(" ");
+        client.tips.set(game, tips);
         message.reply(`${key} à até modifié avec comme valeur: ${value.join(" ")}`);
       } else
       
     
       if (action === "del") {
         if (!key) return message.reply("Merci de préciser la \"Tips\" à supprimer");
-        if (!settings[key]) return message.reply("Cette \"Tips\" n'existe pas");
+        if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
         const response = await client.awaitReply(message, `Etes vous sur de vouloir supprimer ${key}? (oui/non)`);
         if (["o", "oui", "ouais","y","yes"].includes(response)) {
-          delete eft[key];
-          this.client.settings.set(message.guild.id, settings);
+          delete tips[key];
+          this.client.tips.set(game, settings);
           message.reply(`${key} à bien été supprimer.`);
         } else
         if (["n","no","non"].includes(response)) {
@@ -37,10 +38,10 @@ exports.run = async (client, message, [action, key, ...value], level) => { // es
     
       if (action === "view") {
         if (!key) return message.reply("Merci de préciser la \"Tips\" que tu veux consulter");
-        if (!settings[key]) return message.reply("Cette \"Tips\" n'existe pas");
+        if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
         message.reply(`la valeur de ${key} est actuellement ${settings[key]}`);
       } else {
-        message.channel.send(inspect(settings), {code: "json"});
+        message.channel.send(inspect(tips), {code: "json"});
       }
 
     };
@@ -55,7 +56,7 @@ exports.run = async (client, message, [action, key, ...value], level) => { // es
     exports.help = {
       name: "eft",
       category: "Escape from Tarkov",
-      description: "permet d'ajouter, de supprimer et de consulter des tips pour escapes from tarkov ",
+      description: "permet d'ajouter, de supprimer et de consulter des tips pour Escape from Tarkov ",
       usage: "eft <add/view/del/list [nom du tips]>"
     };
        
