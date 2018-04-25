6175bba799867c4c818d21a6c2f1c01231724a0f
swtch <romar@semvolcans.com>
2018-04-25 16:52:50 +0200

    correction

--- commands/eft.js
+++ commands/eft.js
@@ -3,75 +3,74 @@ exports.run = async (client, message, [action, key, ...value], level) => { // es
     const Discord = require("discord.js");
     const game = 'tarkov';
     const tips = client.tips.get(game)
-    
+
 
     if (action === "add") {
-        
+
         if (key == 0) return message.reply("Merci de préciser le \"Tips\" à ajouter");
         if (tips[key]) return message.reply("Cette \"Tips\" existe déja");
         if (value.length < 1) return message.reply("Erreur, aucun contenu specifiée :: eft add titre contenu");
-        tips[key] = {"name": key, "author" : `<@!${message.author.id}>` , "content": value.join(" "), "timestamp" : message.createdAt };
+        tips[key] = { "name": key, "author": `<@!${message.author.username}>`, "content": value.join(" "), "timestamp": message.createdAt , "avatarURL" : message.author.avatarURL};
         client.tips.set(game, tips);
         message.channel.send(`:white_check_mark: **${key}** a bien été ajouté avec comme contenu: \`\`\`${value.join(" ")}\`\`\``);
-      } else
+    } else
 
-      if (action === "edit") {
-        if (!key) return message.reply("Merci de préciser la \"Tips\" à editer");
-        if (value.length < 1) return message.reply("Erreur, aucune valeur specifiée");
-        if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
-        tips[key] = {"name": key, "author" : `<@!${message.author.id}>` , "content": value.join(" "), "timestamp" : message.createdAt };
-        client.tips.set(game, tips);
-        message.channel.send(`:white_check_mark: ${key} à até modifié par le contenu suivant: \`\`\`${value.join(" ")}\`\`\`}`);
-      } else
-      
-    
-      if (action === "del") {
-        if (!key) return message.reply("Merci de préciser la \"Tips\" à supprimer");
-        if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
-        const response = await client.awaitReply(message, `Etes vous sur de vouloir supprimer ${key}? (oui/non)`);
-        if (["o", "oui", "ouais","y","yes"].includes(response)) {
-            message.reply(`${key} à bien été supprimer.`);
-            tips.delete(key)
-          this.client.tips.set(game, tips);
-          message.reply(`${key} à bien été supprimer.`);
+        if (action === "edit") {
+            if (!key) return message.reply("Merci de préciser la \"Tips\" à editer");
+            if (value.length < 1) return message.reply("Erreur, aucune valeur specifiée");
+            if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
+            tips[key] = { "name": key, "author": `<@!${message.author.username}>`, "content": value.join(" "), "timestamp": message.createdAt , "avatarURL" : message.author.avatarURL};
+            client.tips.set(game, tips);
+            message.channel.send(`:white_check_mark: ${key} à até modifié par le contenu suivant: \`\`\`${value.join(" ")}\`\`\`}`);
         } else
-        if (["n","no","non"].includes(response)) {
-          message.reply("Action annulé.");
-        }
-      } else
-      
-    
-      if (action === "view") {
-        if (!key) return message.reply("Merci de préciser la \"Tips\" que tu veux consulter");
-        if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
-        const theTips = tips[key]
-        const embed = new Discord.RichEmbed()
-            .setTitle(String(key.toUpperCase()))
-            .setAuthor(theTips.name)
-            .setColor(0x524918)
-            //.setThumbnail(imgURL)
-            .setTimestamp(theTips.timestamp)
-            //.setURL("https://pubgtracker.com/profile/pc/" + pubgID + "?region=" + serv)
-            .addField(":information_source: ", theTips.content, true);
-       
-        message.channel.send({ embed });
-      } else {
-        message.channel.send(inspect(tips), {code: "json"});
-      }
 
-    };
-    
-    exports.conf = {
-      enabled: true,
-      guildOnly: true,
-      aliases: ["tarkov", "escape"],
-      permLevel: "User"
-    };
-      
-    exports.help = {
-      name: "eft",
-      category: "Escape from Tarkov",
-      description: "permet d'ajouter, de supprimer et de consulter des tips pour Escape from Tarkov ",
-      usage: "eft <add/view/edit/del [nom du tips]>"
-    };
-       
+
+            if (action === "del") {
+                if (!key) return message.reply("Merci de préciser la \"Tips\" à supprimer");
+                if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
+                const response = await client.awaitReply(message, `Etes vous sur de vouloir supprimer ${key}? (oui/non)`);
+                if (["o", "oui", "ouais", "y", "yes"].includes(response)) {
+                    message.reply(`${key} à bien été supprimer.`);
+                    delete tips[key];
+                    client.tips.set(game, tips);
+                    message.reply(`${key} à bien été supprimer.`);
+                } else
+                    if (["n", "no", "non"].includes(response)) {
+                        message.reply("Action annulé.");
+                    }
+            } else
+
+
+                if (action === "view") {
+                    if (!key) return message.reply("Merci de préciser la \"Tips\" que tu veux consulter");
+                    if (!tips[key]) return message.reply("Cette \"Tips\" n'existe pas");
+                    const theTips = tips[key]
+                    const embed = new Discord.RichEmbed()
+                        .setTitle(String(key.toUpperCase()))
+                        .setAuthor(theTips.name)
+                        .setColor(0x524918)
+                        //.setThumbnail(imgURL)
+                        .setTimestamp(theTips.timestamp)
+                        //.setURL("https://pubgtracker.com/profile/pc/" + pubgID + "?region=" + serv)
+                        .addField(":information_source: ", theTips.content, true);
+
+                    message.channel.send({ embed });
+                } else {
+                    message.channel.send(inspect(tips), { code: "json" });
+                }
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ["tarkov", "escape"],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "eft",
+    category: "Escape from Tarkov",
+    description: "permet d'ajouter, de supprimer et de consulter des tips pour Escape from Tarkov ",
+    usage: "eft <add/view/edit/del [nom du tips]>"
+};
